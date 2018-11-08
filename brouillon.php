751ed9d9c72618cb9ed6fb7b0739eab90489cc71
gen-013 <dcl.sylvieg@18pixel.fr>
2018-11-08 20:15:11 +0100

    design et fonctionnalité ok

---
+++ brouillon.php
@@ -0,0 +1,227 @@
+        <table>
+                <div id="col1">
+                    <h2> Voici la table de <?php echo $_GET["tables"] ?></h2>
+                    
+                    <!-- <tr>
+                        <td> <?php echo $_GET["tables"] ?> </td>
+                        <td> x </td>
+                        <td> 0 </td>
+                        <td> = </td>
+                        <td> <?php echo ($_GET["tables"]*0) ?> </td>
+                    </tr> -->
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  1 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*1) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  2 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*2) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  3 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*3) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  4 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*4) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  5 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*5) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  6 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*6) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  7 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*7) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  8 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*8) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td>  9 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*9) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 10 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*10) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 11 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*11) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 12 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*12) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 13 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*13) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 14 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*14) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 15 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*15) ?></td>
+                    </tr>
+                </div>   
+                <div id="col2">   
+                    
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 16 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*16) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 17 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*17) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 18 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*18) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 19 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*19) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 20 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*20) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 21 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*21) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 22 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*22) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 23 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*23) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 24 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*24) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 25 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*25) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 26 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*26) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 27 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*27) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 28 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*28) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 29 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*29) ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo $_GET["tables"] ?></td>
+                        <td> x </td>
+                        <td> 30 </td>
+                        <td> = </td>
+                        <td><?php echo ($_GET["tables"]*30) ?></td>
+                    </tr>
+                </div>
+
+            </table>
--- table.php
+++ table.php
@@ -1,227 +1,54 @@
-        <table>
-                <div id="col1">
-                    <h2> Voici la table de <?php echo $_GET["tables"] ?></h2>
-                    
-                    <!-- <tr>
-                        <td> <?php echo $_GET["tables"] ?> </td>
-                        <td> x </td>
-                        <td> 0 </td>
-                        <td> = </td>
-                        <td> <?php echo ($_GET["tables"]*0) ?> </td>
-                    </tr> -->
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  1 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*1) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  2 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*2) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  3 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*3) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  4 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*4) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  5 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*5) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  6 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*6) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  7 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*7) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  8 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*8) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td>  9 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*9) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 10 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*10) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 11 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*11) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 12 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*12) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 13 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*13) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 14 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*14) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 15 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*15) ?></td>
-                    </tr>
-                </div>   
-                <div id="col2">   
-                    
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 16 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*16) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 17 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*17) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 18 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*18) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 19 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*19) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 20 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*20) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 21 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*21) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 22 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*22) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 23 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*23) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 24 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*24) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 25 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*25) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 26 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*26) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 27 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*27) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 28 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*28) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 29 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*29) ?></td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $_GET["tables"] ?></td>
-                        <td> x </td>
-                        <td> 30 </td>
-                        <td> = </td>
-                        <td><?php echo ($_GET["tables"]*30) ?></td>
-                    </tr>
-                </div>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Table de multiplication</title>
+</head>
+<body>
+    <div id="bloc_page">
 
-            </table>
+        <h1>les tables de multiplication</h1>
+
+
+        <div id="bloc2">
+
+            <?php include "formulaire.html" ?>
+   
+            <div id="table">
+                <h2> Voici la table de <?php echo $_GET["tables"] ?></h2>
+                <div id="bloc3">
+                    <div class="col">
+                            
+                        <?php 
+                            for($a=0; $a<10; $a++){
+                                echo $_GET["tables"].' x '.$a.' = '.($_GET["tables"]*$a).'<br/>';
+                            }
+                        ?>
+                    </div>  
+                    <div class="col">
+                        <?php 
+                            for($a=10; $a<20; $a++){
+                               echo $_GET["tables"].' x '.$a. ' = '.($_GET["tables"]*$a).'<br/>';
+                            }
+                        ?>
+                    </div>
+                    <div class="col">
+                        <?php 
+                            for($a=20; $a<30; $a++){
+                                echo $_GET["tables"].' x '.$a. ' = '.($_GET["tables"]*$a).'<br/>';
+                            }
+                        ?>
+                    </div>
+                        
+
+                </div>
+            </div>
+
+
+
+<script src="js/script.js"></script>                          
+</body>
+</html>
